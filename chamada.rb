a8a64496d04e57b6d40f96f448d163d051c6eda8
Prof. Márcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2024-07-16 19:18:14 -0300

    Create chamada.rb

---
+++ chamada.rb
@@ -0,0 +1,43 @@
+# ruby
+presentes = %w{
+  Luã
+  David
+  Bruno
+  Vinícius
+  Gustavo
+  Evelyn
+  Douglas
+  JoãoS
+  Ronaldo
+  Felipe
+  Fellipe
+  Alan
+  Jaifer
+  Pablo
+  Arthur
+  Lucas
+  Mathias
+  Adriano
+  Daniel
+  Julia
+  JoãoB
+}
+
+presentes.shuffle!
+
+until presentes.empty?
+  p presentes.pop(2)
+end
+
+# vscode ➜ /workspaces/ruby $ ruby chamada.rb 
+# ["Jaifer", "Luã"]
+# ["Lucas", "Gustavo"]
+# ["Pablo", "David"]
+# ["Felipe", "Julia"]
+# ["Arthur", "JoãoB"]
+# ["Alan", "Daniel"]
+# ["Mathias", "Douglas"]
+# ["Vinícius", "JoãoS"]
+# ["Evelyn", "Ronaldo"]
+# ["Bruno", "Fellipe"]
+# ["Adriano", "Matheus"]
